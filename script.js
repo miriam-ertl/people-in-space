03c2c80cf77ab2e983e4816363bc16b52abe36ec
Miriam Ertl <miriam_ertl@web.de>
2023-08-17 18:45:01 +0200

    testing console.log

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const peopleInSpace = document.querySelector(`[data-js="block-element"]`);
+
+async function fetchPeople() {
+  console.log("hello");
+  const response = await fetch("http://api.open-notify.org/astros.json");
+  console.log(response);
+  const peopleData = number();
+  console.log(peopleData);
+}
